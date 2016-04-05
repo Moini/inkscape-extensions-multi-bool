55aba329baa07523a9cbfce1efdbb77aede794ea
Moini <Moini@users.noreply.github.com>
2016-04-05 19:46:56 +0200

    Add Licence

--- multipledifference.py
+++ multipledifference.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+
+#    Inkscape Multi-Bool extension for Inkscape 0.91
+#    Copyright (C) 2014  Ryan Lerch (multiple difference)
+#                  2016  Maren Hachmann (refactoring, extend to multibool)
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 """
 This extension takes a selection of paths, and applies a difference
 between the top shape in the z-order, and each of the shapes underneath.
